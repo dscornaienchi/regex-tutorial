bf7c2c0af176a75809701e3d7d4a18b9f8feffeb
dscornaienchi <Dscorni32@gmail.com>
2023-11-27 18:35:12 -0600

    debugged code to deploy to localhost

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost/social-network', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+mongoose.connect('mongodb://localhost:27017/social-network', {
+  //useFindAndModify: false,
+  //useNewUrlParser: true,
+  //useUnifiedTopology: true
 });
 
 app.use(routes);
 
-mongoose.set('useCreateIndex', true);
+//mongoose.set('useCreateIndex', true);
 
 mongoose.connection.once('open', () => {
   console.log('Connected to MongoDB database');
